030fbe031d8b9cc59cf75ccb471d82b94f918dd3
sanketh908 <sankeths908@gmail.com>
2026-02-18 15:07:01 +0530

    ALL DONE

--- src/main/java/com/Sanketh/WeatherApp/Controller/frontendController.java
+++ src/main/java/com/Sanketh/WeatherApp/Controller/frontendController.java
@@ -1,7 +1,35 @@
 package com.Sanketh.WeatherApp.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.ui.Model;
+import com.Sanketh.WeatherApp.Entity.WeatherResponse;
+import com.Sanketh.WeatherApp.Service.WetherService;
+import org.springframework.stereotype.Controller;
 
-@CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class frontendController {
+    private final WetherService wetherService;
+
+    public frontendController(WetherService wetherService) {
+        this.wetherService = wetherService;
+    }
+
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @PostMapping("/getwether")
+    public  String getWether(@RequestParam String city,Model model) {
+       WeatherResponse weatherResponse=  wetherService.getWeather(city);
+        WeatherResponse response = wetherService.getWeather(city);
+        model.addAttribute("weather", response);
+        model.addAttribute("city", city);
+        return "index";
+
+    }
+
 }
